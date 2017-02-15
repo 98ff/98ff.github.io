58c292f7b6c8de24c6ab09a9daf49d6aa601831b
ying <1243103680@qq.com>
2017-02-15 18:06:00 +0800

    支持链式调用带参数的插件封装完

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 //只要轮播图的根标签(任何选择器)
-$('.slide').zySlide({speed:3000});
-$('#slide').zySlide({delay:500,speed:1500});
+$('.slide').zySlide({speed:3000}).css('background-color','black');
+$('#slide').zySlide({delay:500,speed:1500}).css({
+	'border': '2px solid black',
+	'background-color':'gray'
+});
